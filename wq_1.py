8e3a4c392cde1fe0efb35da95f0b658b41ea19cb
李富荣 <leefurong@gmail.com>
2019-10-06 23:41:47 +0800

    实现了栅格定位

--- wq_1.py
+++ wq_1.py
@@ -13,8 +13,30 @@ qizi = []
 WIDTH = qipan.width
 HEIGHT = qipan.height
 
+
+# 经过试验， 左上角(1, 1)和右下角(19, 19)的坐标分别为：
+# (17, 17)  (607, 607)
+MIN = 17.0
+MAX = 607.0
+CELL = (MAX-MIN)/18
+
+
+def x2g(x):
+    """把棋盘上的像素坐标转换为棋盘的栅格坐标"""
+    return int(round((x - MIN) / CELL))+1
+
 def pos2grid(pos):
-    print(pos)
+    return (x2g(pos[0]), x2g(pos[1]))
+
+def g2x(g):
+    """把棋盘的栅格坐标转换为像素坐标"""
+    return (g-1) * CELL + MIN
+
+def grid2pos(grid):
+    return (g2x(grid[0]), g2x(grid[1]))
+
+def improvePos(pos):
+    return grid2pos(pos2grid(pos))
 
 def draw():
     qipan.draw()
@@ -26,11 +48,12 @@ def draw():
         aQizi.draw()
 
 def on_mouse_move(pos):
+    pos = improvePos(pos)
     black.center = pos
     white.center = pos
 
 def on_mouse_down(pos, button):
-    pos2grid(pos)
+    pos = improvePos(pos)
     if button == mouse.LEFT:
         if kexia():
             luozi(pos)
